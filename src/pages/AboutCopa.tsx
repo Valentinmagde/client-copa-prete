cd8894af6f85d0b48905f361d9b281714d5f2d27
Valentinmagde <valentinmagde@gmail.com>
2026-03-20 16:47:35 +0400

    Update profile

--- src/pages/AboutCopa.tsx
+++ src/pages/AboutCopa.tsx
@@ -7,7 +7,7 @@ import Footer from "../components/layout/Footer";
 import Slider from "react-slick";
 import RowBgImg from "../assets/img/row-bgimage-1.png";
 import DottedShapeImg  from "../assets/img/dotted_shape.png";
-import about1 from "../assets/img/about/copa/01.jpg";
+import about1 from "../assets/img/about/copa/02.jpg";
 import about2 from "../assets/img/about/04.png";
 import about3 from "../assets/img/about/05.png";
 
